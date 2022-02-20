336d3719a1114ad536f67a3a2f91930456862c57
Francisco Grande <fgrande@mobydigital.com>
2022-02-20 11:54:07 -0300

    Change base path swagger

--- src/main/java/com/challenge/pokemon/config/SwaggerConfig.java
+++ src/main/java/com/challenge/pokemon/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.challenge.pokemon"))
-                .paths(PathSelectors.any())
+                .paths(PathSelectors.ant("https://api-pokemon.azurewebsites.net/"))
                 .build()
                 .apiInfo(apiInfo());
     }
