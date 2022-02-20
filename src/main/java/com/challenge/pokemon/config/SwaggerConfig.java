77e2ef1728505fe3b8dc49990cc1e0109089565f
Francisco Grande <fgrande@mobydigital.com>
2022-02-20 11:32:16 -0300

    Bugfix config swagger

--- src/main/java/com/challenge/pokemon/config/SwaggerConfig.java
+++ src/main/java/com/challenge/pokemon/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.challenge.apipokemon"))
+                .apis(RequestHandlerSelectors.basePackage("com.challenge.pokemon"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
