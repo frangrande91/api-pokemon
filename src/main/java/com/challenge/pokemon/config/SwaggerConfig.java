3d3722294290b7b2ac54f2ee5c63401844b917d2
Francisco Grande <fgrande@mobydigital.com>
2022-02-20 18:52:28 -0300

    Added host in SwaggerConfig

--- src/main/java/com/challenge/pokemon/config/SwaggerConfig.java
+++ src/main/java/com/challenge/pokemon/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.challenge.pokemon.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.PathProvider;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,9 +18,10 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host("https://api-pokemon.azurewebsites.net")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.challenge.pokemon"))
-                .paths(PathSelectors.ant("https://api-pokemon.azurewebsites.net/"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
