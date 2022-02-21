472f4a76f1fd12578a66adca2861dddc8ce7778c
Francisco Grande <fgrande@mobydigital.com>
2022-02-20 23:36:31 -0300

    Bugfix host swagger config

--- src/main/java/com/challenge/pokemon/config/SwaggerConfig.java
+++ src/main/java/com/challenge/pokemon/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.challenge.pokemon.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.PathProvider;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -18,7 +17,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("https://api-pokemon.azurewebsites.net")
+                .host("api-pokemon.azurewebsites.net")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.challenge.pokemon"))
                 .paths(PathSelectors.any())
